3bc1250628dc5758a4ca89e819e074e1c5233e94
Vilya Harvey <vilya.harvey@gmail.com>
2011-12-23 21:26:39 +0000

    Added a random level generation function.

--- src/level.cpp
+++ src/level.cpp
@@ -1,6 +1,8 @@
 #include "level.h"
 
 #include <algorithm>
+#include <cmath>
+#include <sstream>
 
 namespace cat {
 
@@ -55,8 +57,73 @@ namespace cat {
 
   bool RandomLevel(Level& level)
   {
-    // TODO
-    return false;
+    const std::string firstWord[] = {
+      "Random",
+      "Quantum",
+      "Imaginary",
+      "Strange",
+      "Weird",
+      "Happy"
+    };
+    const std::string secondWord[] = {
+      "Level",
+      "World",
+      "Science",
+      "Feline",
+      "Stuff",
+      "Cat"
+    };
+    const double kNormalThresh = 0.95;
+    const double kSuperpositionThresh = 0.99;
+
+    int firstIndex = int(drand48() * 6) % 6;
+    int secondIndex = int(drand48() * 6) % 6;
+    std::ostringstream name;
+    name << firstWord[firstIndex] << " " << secondWord[secondIndex];
+    
+    int numAtoms = int(drand48() * kMaxAtoms) % kMaxAtoms;
+    double emitFrequency = drand48() * 1000.0;
+
+    level.name = name.str();
+    level.duration = emitFrequency * numAtoms + 5000.0;
+    level.maxAtomCount = 0;
+    
+    for (int i = 0; i < numAtoms; ++i) {
+      double typeVal = drand48();
+      AtomType type = eAtomNormal;
+      if (typeVal > kNormalThresh)
+        type = (typeVal > kSuperpositionThresh) ? eAtomEntanglement : eAtomSuperposition;
+
+      // Random emit position along any wall.
+      int wall = int(drand48() * 4) % 4;
+      float angle = (drand48() * 0.9 + 0.05) * M_PI; // in radians, 0 is parallel to +ve x axis, pi/2 is +ve y axis
+      float speed = drand48() * 0.003 + 0.001; // Random speed between 0.001 and 0.004
+
+      Vec2 pos;
+      switch (wall) {
+      case 0: // left wall
+        pos = Vec2(0, drand48());
+        angle += M_PI_2;
+        break;
+      case 1: // top wall
+        pos = Vec2(drand48(), 1);
+        angle += M_PI;
+        break;
+      case 2: // right wall
+        pos = Vec2(1, drand48());
+        angle -= M_PI_2;
+        break;
+      case 3: // bottom wall
+        pos = Vec2(drand48(), 0);
+        break;
+      }
+
+      Vec2 vel = Vec2(cos(angle), sin(angle)) * speed;
+
+      level.addAtom(type, (i + 1) * emitFrequency, pos, vel);
+    }
+
+    return true;
   }
 
 
